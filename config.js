79d808b798090bad18c716ed3f4506ba06c6099d
BrianOverkill <BrianOverkill71@gmail.com>
2025-03-29 23:31:06 -0700

    update

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    // Get your API key from: https://openweathermap.org/api
+    API_KEY: 'YOUR_API_KEY_HERE',
+    BASE_URL: 'https://api.openweathermap.org/data/2.5/weather'
+};
